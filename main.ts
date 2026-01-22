90f786c1080f0fe9bd86a1a8925045de7a33b90c
OKYESMATE <lochlan@magrin.com>
2026-01-22 12:10:44 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 //% color=#00AACC icon="\uf11b"
-namespace JoystickInput {
+namespace Joy-Bit {
 
+    let oldX = 0
+    let oldY = 0
+    
     //% block="read analog pin %pin"
     //% pin.defl=AnalogPin.P0
     export function readAnalog(pin: AnalogPin): number {
         return pins.analogReadPin(pin)
     }
 
-    //% block="read digital pin %pin"
+    //% block="read button pin %pin"
     //% pin.defl=DigitalPin.P2
     export function readButton(pin: DigitalPin): boolean {
         return pins.digitalReadPin(pin) == 1
@@ -24,4 +27,11 @@ namespace JoystickInput {
     export function joystickY(pin: AnalogPin): number {
         return pins.analogReadPin(pin)
     }
+
+    export function hasStickMoved(): boolean {
+        return (oldX != pins.analogReadPin(AnalogPin.P0) || oldY != pins.analogReadPin(AnalogPin.P1))
+    }
+
+    let oldX = pins.analogReadPin(AnalogPin.P0)
+    let oldy = pins.analogReadPin(AnalogPin.P1)
 }
